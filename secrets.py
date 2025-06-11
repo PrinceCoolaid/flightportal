1e60feb4bda89d1b98832ca507d061821bcf99d7
PrinceCoolaid <pierce.cooley@gmail.com>
2025-06-10 20:33:56 -0700

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -2,8 +2,8 @@
 # If you put them in the code you risk committing that info or sharing it
 
 secrets = {
-    'ssid' : 'wifi network',
-    'password' : 'wifi password',
+    'ssid' : 'Mr. Macaroni',
+    'password' : 'Texas3620',
     # area to search for flights: top latitude, bottom latitude, left longitude, right longitude
     # (so this example is central London)
     'bounds_box' : '51.6,51.4,-0.3,-0.1'
